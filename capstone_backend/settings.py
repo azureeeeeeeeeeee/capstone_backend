16ec4ef5d1abc83adef5f4e3fc42176258f3441d
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2025-12-14 23:16:59 +0800

    add

--- capstone_backend/settings.py
+++ capstone_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$&)0j)p8rfxbl4)bo1+b=q=t88yw@eja*=e@0&_jhn)z%jw*1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.2.2", "localhost"]
+ALLOWED_HOSTS = ["10.0.2.2", "localhost", "127.0.0.1"]
 
 
 # Application definition
