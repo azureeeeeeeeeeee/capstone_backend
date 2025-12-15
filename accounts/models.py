1a86d0910622b91e9153d4f2ee08cb37a4f0ffad
meeptaquelle <asyharmiftahul123@gmail.com>
2025-12-15 15:20:49 +0800

    fix error get periode and stuff

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 class Role(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
--- api/permissions/permissions.py
+++ api/permissions/permissions.py
@@ -8,26 +8,30 @@ class SurveyPermissions(BasePermission):
 
 class ProgramSpecificQuestionPermissions(BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated or not request.user.role:
+        user = request.user
+        if not user.is_authenticated or not user.role:
             return False
 
-        role_name = request.user.role.name
+        role = user.role.name
+
+        # Semua boleh lihat
         if request.method == 'GET':
             return True
 
-        if role_name in ['Admin', 'Tracer']:
+        # Admin & Tracer full akses
+        if role in ['Admin', 'Tracer']:
             return True
 
-        if role_name == 'Tim Prodi':
+        # Tim Prodi hanya prodi sendiri
+        if role == 'Tim Prodi':
             program_study_id = view.kwargs.get('program_study_id')
-            if not program_study_id:
-                return False
             return (
-                request.user.program_study
-                and str(request.user.program_study.id) == str(program_study_id)
+                user.program_study
+                and str(user.program_study.id) == str(program_study_id)
             )
 
         return False
+
 
     
 
@@ -37,9 +41,18 @@ class UnitPermissions(BasePermission):
             return request.user.is_authenticated and request.user.role.name in ['Admin']
         return True
     
+# class PeriodePermissions(BasePermission):
+#     def has_permission(self, request, view):
+#         return request.user.is_authenticated and request.user.role.name in ['Admin', 'Tracer']
 class PeriodePermissions(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role.name in ['Admin', 'Tracer']
+        if request.method == 'GET':
+            return request.user.is_authenticated
+        return (
+            request.user.is_authenticated
+            and request.user.role.name in ['Admin', 'Tracer']
+        )
+
     
 class RolePermissions(BasePermission):
     def has_permission(self, request, view):
