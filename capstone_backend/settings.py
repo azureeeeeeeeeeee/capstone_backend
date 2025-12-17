1ef878ce122cc330a0af2b86fe7043096cc3fb35
FakhrizalN <fakhrizal566@gmail.com>
2025-12-17 11:08:12 +0800

    Update settings.py

--- capstone_backend/settings.py
+++ capstone_backend/settings.py
@@ -166,6 +166,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.8:3000",  # Physical Device (update IP sesuai network Anda)
 ]
 
+# CORS_ALLOW_ALL_ORIGINS = True
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
